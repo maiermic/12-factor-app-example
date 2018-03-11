c859b4d94b70cf90c984c45cd2202668c6af2e10
Michael Maier <maier1michael@gmail.com>
2018-03-11 21:28:01 +0100

    configure frontend

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-export FILE_STORAGE_PORT=3001
+export HOST_IP=$(ip route get 1 | awk '{print $NF;exit}')
 export BACKEND_PORT=3000
+export FILE_STORAGE_PORT=3001
+export FRONTEND_PORT=3002
 export VISUALIZER_PORT=8080
-export BACKEND_BASE_IMAGE_URL="http://$(hostname -i):${FILE_STORAGE_PORT}"
+export BACKEND_BASE_IMAGE_URL="http://${HOST_IP}:${FILE_STORAGE_PORT}"
+export BACKEND_URL="http://${HOST_IP}:${BACKEND_PORT}"
 docker stack deploy -c docker-compose.yml 12-factor-app-example
